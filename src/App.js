cc0c57b16f7e61894e7abd7ae9a6727cecc67b27
Dagwal <dagimwalelegne@gmail.com>
2023-12-17 15:49:58 +0300

    Updating package.json

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 // src/App.js
 import React from "react";
-import "@mantine/core/styles.css";
-import { MantineProvider } from "@mantine/core";
 import SampleUI from "./components/NavbarSimple";
 
 function App() {
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS>
       <SampleUI />
-    </MantineProvider>
   );
 }
 
